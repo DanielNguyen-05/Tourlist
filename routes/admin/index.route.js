6091f80abbb16f4a3a4a219e6e9f7f2623f4f26b
DanielNguyen-05 <nhdang231@clc.fitus.edu.vn>
2025-04-16 13:35:38 +0700

    Done the Dashboard page

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,9 +1,10 @@
 // this is the final file route to store all the routes of admin
 const router = require('express').Router(); 
 const accountRoutes = require("./account.route");
-
+const dashboardRoutes = require("./dashboard.route");
 
 router.use('/account', accountRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
 
