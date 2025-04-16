c4e5618ed7c77adaf3e2d1c3ef7be2d4575deea4
DanielNguyen-05 <nhdang231@clc.fitus.edu.vn>
2025-04-16 14:45:32 +0700

    Done the contact-list page

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -6,6 +6,7 @@ const categoryRoutes = require("./category.route");
 const tourRoutes = require("./tour.route");
 const orderRoutes = require("./order.route");
 const userRoutes = require("./user.route");
+const contactRouts = require("./contact.route");
 
 router.use('/account', accountRoutes);
 router.use('/dashboard', dashboardRoutes);
@@ -13,6 +14,7 @@ router.use('/category', categoryRoutes);
 router.use('/tour', tourRoutes);
 router.use('/order', orderRoutes);
 router.use('/user', userRoutes);
+router.use('/contact', contactRouts);
 
 module.exports = router;
 
